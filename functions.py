b08eb59bb789800620e58340bbbf4a22e697ce85
Ibragim <ibragim.aseev@icloud.com>
2022-11-23 14:51:37 +0300

    fix coordinates bug

--- functions.py
+++ functions.py
@@ -1,14 +1,13 @@
 import math
 
 
-# fix it
 def get_coordinates(first, second, third):
-    left_side_x = 0.25 + 0.25 * (first - second)
-    left_side_y = (math.sqrt(3) / 2) - math.sqrt(3) * abs(left_side_x - 0.5)
+    left_side_x = 0.25 + 0.25 * (third - first)
+    left_side_y = 1 - 2 * abs(left_side_x - 0.5)
     left_side = ((left_side_x, left_side_y), (1, 0))
 
-    right_side_x = 0.75 + 0.25 * (third - second)
-    right_side_y = (math.sqrt(3) / 2) - math.sqrt(3) * abs(right_side_x - 0.5)
+    right_side_x = 0.75 + 0.25 * (second - third)
+    right_side_y = 1 - 2 * abs(right_side_x - 0.5)
     right_side = ((right_side_x, right_side_y), (0, 0))
 
     y = (first + second + third) / 3
@@ -17,8 +16,10 @@ def get_coordinates(first, second, third):
     return x, y, z
 
 
-# fix it
 def lines_intersection(line1, line2):
+    if line1[0] == line2[1] and line1[1] == line2[0]:
+        return 0.5, 0
+
     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
